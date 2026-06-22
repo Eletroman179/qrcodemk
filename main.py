916f0b05440e4e3e4751f5e81cf2b88c394d2999
Electroman179 <jameschaowadee@gmail.com>
2026-06-22 21:06:07 +0700

    hotfix 1

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
 import qrcode 
+import sys
 
-img = qrcode.make(input("qrcode data: "))
+def get_data():
+    if sys.stdin.isatty():
+        return input("qrcode data: ")
+    else:
+        # if piped, force interactive fallback
+        print("qrcode data: ", end="", flush=True)
+        return sys.stdin.readline().strip()
+
+data = get_data()
+
+img = qrcode.make(data)
 print("saved qrcode as qrcode.png")
 img.save("qrcode.png") # type: ignore
